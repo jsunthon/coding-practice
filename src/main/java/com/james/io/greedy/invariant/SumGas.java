ffef3e624a473fcced6dfd46c654bc64bfcf98e4
--global <--global>
2019-04-16 08:31:23 -0700

    gas summing problem 18.6 of EPI

---
+++ src/main/java/com/james/io/greedy/invariant/SumGas.java
@@ -0,0 +1,35 @@
+package com.james.io.greedy.invariant;
+
+import java.util.List;
+
+/**
+ * 18.6 Gas Summing Problem
+ *
+ * Reduce from O(n^2) time complexity to O(n)
+ */
+public class SumGas {
+    private static final int MPG = 20;
+
+    private static class CityAndGas {
+        int city;
+        int gallons;
+
+        CityAndGas(int city, int gallons) {
+            this.city = city;
+            this.gallons = gallons;
+        }
+    }
+
+    public static int getAmple(List<Integer> gallons,
+                               List<Integer> distances) {
+        CityAndGas min = new CityAndGas(0,0 );
+        int remainingGallons = 0;
+        for (int i = 1; i < gallons.size(); i++) {
+            remainingGallons += gallons.get(i - 1) - distances.get(i - 1) / MPG;
+            if (remainingGallons < min.gallons) {
+                min = new CityAndGas(i, remainingGallons);
+            }
+        }
+        return min.city;
+    }
+}
