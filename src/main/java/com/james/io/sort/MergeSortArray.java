78c5bfb37ab291dcc5deeedc29c8c71c44a43072
unknown <jsunthon@gmail.com>
2019-04-26 20:11:35 -0700

    code my solution to merge two sorted array in epi

---
+++ src/main/java/com/james/io/sort/MergeSortArray.java
@@ -0,0 +1,55 @@
+package com.james.io.sort;
+
+import java.util.List;
+
+/**
+ * Custom, non-efficient solution to problem 14.2 of EPI
+ */
+public class MergeSortArray {
+
+    // O(m * n) time complexity, O(1) space complexity
+    public static void merge(List<Integer> list1, List<Integer> list2) {
+        for (int i = 0; i < list2.size(); i++) {
+            int curr = list2.get(i);
+            int index = getIndex(curr, list1);
+            fill(curr, index, list1);
+        }
+    }
+
+    // EPI solution: O(m + n) time complexity, O(1) space complexity
+    public static void mergeEpi(List<Integer> list1,
+                                List<Integer> list2) {
+        int a = list1.size() - 1;
+        int b = list2.size() - 1;
+        int writeIdx = list1.size() + list2.size() - 1;
+        while (a >= 0 && b >= 0) {
+            list1.set(writeIdx--, list1.get(a) > list2.get(b) ? list1.get(a--) : list2.get(b--));
+        }
+        while (b >= 0) {
+            list1.set(writeIdx--, list2.get(b--));
+        }
+    }
+
+    private static int getIndex(int value, List<Integer> list) {
+        int i = 0;
+
+        while (i < list.size() && list.get(i) != null) {
+            if (value <= list.get(i)) {
+                return i;
+            }
+            i++;
+        }
+
+        return i;
+    }
+
+    private static void fill(int value, int index, List<Integer> list) {
+        Integer curr = value;
+        Integer temp;
+        for (int i = index; i < list.size() && curr != null; i++) {
+            temp = list.get(i);
+            list.set(i, curr);
+            curr = temp;
+        }
+    }
+}
---
+++ src/test/java/com/james/io/sort/MergeSortArrayTest.java
@@ -0,0 +1,33 @@
+package com.james.io.sort;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+public class MergeSortArrayTest {
+
+    @Test
+    public void testMerge() {
+        List<Integer> list1 = Stream.of(5, 13, 17, null, null, null, null, null).collect(Collectors.toList());
+        List<Integer> list2 = Stream.of(3, 7, 11, 19).collect(Collectors.toList());
+        List<Integer> expectedRes = Stream.of(3, 5, 7, 11, 13, 17, 19, null).collect(Collectors.toList());
+        MergeSortArray.merge(list1, list2);
+        assertEquals(expectedRes, list1);
+    }
+
+    @Ignore
+    @Test
+    public void testMergeEpi() {
+        List<Integer> list1 = Stream.of(5, 13, 17, null, null, null, null, null).collect(Collectors.toList());
+        List<Integer> list2 = Stream.of(3, 7, 11, 19).collect(Collectors.toList());
+        List<Integer> expectedRes = Stream.of(3, 5, 7, 11, 13, 17, 19, null).collect(Collectors.toList());
+        MergeSortArray.mergeEpi(list1, list2);
+        assertEquals(expectedRes, list1);
+    }
+}
