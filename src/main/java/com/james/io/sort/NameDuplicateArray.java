1ebe05ece412d88533793752ad51662786fe8741
unknown <jsunthon@gmail.com>
2019-04-27 16:15:55 -0700

    14.3

---
+++ src/main/java/com/james/io/sort/NameDuplicateArray.java
@@ -0,0 +1,50 @@
+package com.james.io.sort;
+
+import com.james.io.sort.model.Name;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Solution for problem 14.3 of EPI
+ */
+public class NameDuplicateArray {
+
+    private final List<Name> names;
+
+    public NameDuplicateArray(List<Name> names) {
+        this.names = names;
+    }
+
+    /**
+     * O(n) time
+     * O(n) space
+     * @return
+     */
+    public Set<Name> eliminateDuplicatesBruteForce() {
+        return names.stream().collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    /**
+     * O(n log n) time
+     * O(1) space
+     */
+    public void eliminateDuplicates() {
+        Collections.sort(names);
+        int lastValidInd = 0;
+        Name lastValidName;
+        Name currName;
+        for (int curr = 1; curr < names.size(); curr++) {
+            lastValidName = names.get(lastValidInd);
+            currName = names.get(curr);
+            if (!currName.getFirstName().equals(lastValidName.getFirstName())) {
+                names.set(++lastValidInd, currName);
+            }
+        }
+        names.subList(lastValidInd + 1, names.size()).clear();
+    }
+
+    public List<Name> getNames() {
+        return names;
+    }
+}
---
+++ src/main/java/com/james/io/sort/model/Name.java
@@ -0,0 +1,77 @@
+package com.james.io.sort.model;
+
+import java.util.Objects;
+
+// immutable name using the builder pattern
+public class Name implements Comparable<Name> {
+    private final String firstName;
+
+    private final String lastName;
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Name build() {
+            return new Name(this);
+        }
+    }
+
+    private Name(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+    }
+
+    @Override
+    public int compareTo(Name other) {
+        int firstNameCmp = firstName.compareTo(other.firstName);
+        if (firstNameCmp != 0) {
+            return firstNameCmp;
+        }
+
+        return lastName.compareTo(other.lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+//    @Override
+//    public boolean equals(Object other) {
+//        if (this == other) {
+//            return true;
+//        }
+//
+//        if (!(other instanceof Name)) {
+//            return false;
+//        }
+//
+//        Name otherName = (Name) other;
+//
+//        return Objects.equals(firstName, otherName.firstName);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hashCode(firstName);
+//    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
