bc4ee195c51352832f7eac5e4693a77a617a8a65
unknown <jsunthon@gmail.com>
2019-04-29 08:38:50 -0700

    solution for 12.1 variant 3

---
+++ src/main/java/com/james/io/search/IntervalSearchSolution.java
@@ -0,0 +1,76 @@
+package com.james.io.search;
+
+import com.james.io.search.model.Interval;
+
+import java.util.List;
+
+/**
+ * Soluition to the 3rd variant of problem 12.1 in EPI
+ */
+public class IntervalSearchSolution {
+
+    // my custom solution
+    public static Interval getInterval(List<Integer> list,
+                                       int key) {
+        int low = 0;
+        int high = list.size() - 1;
+        int midIndex;
+        int midValue;
+
+        while (low <= high) {
+            midIndex = low + (high - low) / 2;
+            midValue = list.get(midIndex);
+
+            if (key > midValue) {
+                low = midIndex + 1;
+            } else if (key == midValue) {
+                return explore(list, midIndex, key);
+            } else {
+                high = midIndex - 1;
+            }
+        }
+
+        return new Interval(-1, -1);
+    }
+
+    private static Interval explore(List<Integer> list,
+                                    int midIndex,
+                                    int key) {
+        final int leftInd = exploreLeft(list, midIndex, key);
+        final int rightInd = exploreRight(list, midIndex, key);
+
+        return new Interval(leftInd, rightInd);
+    }
+
+    private static int exploreLeft(List<Integer> list,
+                                   int midIndex,
+                                   int key) {
+        int res = midIndex;
+        while (midIndex >= 0) {
+            if (list.get(midIndex) == key) {
+                res = midIndex;
+                midIndex--;
+            } else {
+                return res;
+            }
+        }
+
+        return res;
+    }
+
+    private static int exploreRight(List<Integer> list,
+                                    int midIndex,
+                                    int key) {
+        int res = midIndex;
+        while (midIndex <= list.size() - 1) {
+            if (list.get(midIndex) == key) {
+                res = midIndex;
+                midIndex++;
+            } else {
+                return res;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/main/java/com/james/io/search/model/Interval.java
@@ -0,0 +1,42 @@
+package com.james.io.search.model;
+
+public final class Interval {
+    private final int left;
+    private final int right;
+
+    public Interval(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (!(other instanceof Interval)) {
+            return false;
+        }
+
+        final Interval otherInterval = (Interval) other;
+
+        final boolean leftEquals = left == otherInterval.getLeft();
+        final boolean rightEquals = right == otherInterval.getRight();
+
+        return leftEquals && rightEquals;
+    }
+
+    @Override
+    public String toString() {
+        return left + ", " + right;
+    }
+}
