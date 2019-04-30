b075f0c9ff5018c32a786f385548c00d0f60b27d
--global <--global>
2019-04-30 07:26:10 -0700

    12.1 and some skip

---
+++ src/main/java/com/james/io/search/IndexEqualitySearchSolution.java
@@ -0,0 +1,55 @@
+package com.james.io.search;
+
+import java.util.List;
+
+/**
+ * Solution to problem 12.2 of EPI
+ */
+public class IndexEqualitySearchSolution {
+
+    private IndexEqualitySearchSolution() {}
+
+    public static int searchIndexEqualsWithNoDuplicate(List<Integer> list) {
+        int low = 0;
+        int high = list.size() - 1;
+        int mid;
+        int midValue;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            midValue = list.get(mid);
+
+            if (midValue > mid) {
+                high = mid - 1;
+            } else if (midValue == mid) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int searchIndexEqualsWithDuplicate(List<Integer> list) {
+        int low = 0;
+        int high = list.size() - 1;
+        int mid;
+        int midValue;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            midValue = list.get(mid);
+
+            if (midValue == mid) {
+                return mid;
+            } else if (midValue < mid || ((mid < list.size() - 1) && (midValue == list.get(mid + 1)))) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
