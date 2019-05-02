f3522f09c3e6b15179f95df21ff1fe8b1fc62f5c
--global <--global>
2019-05-02 08:06:33 -0700

    variant 1 solution to problem 12.3

--- src/main/java/com/james/io/search/CircularSortedArraySolution.java
+++ src/main/java/com/james/io/search/CircularSortedArraySolution.java
@@ -2,8 +2,12 @@ package com.james.io.search;
 
 import java.util.List;
 
+/**
+ * Problem 12.3 of EPI
+ */
 public class CircularSortedArraySolution {
 
+    // original, epi solution
     public static int getSmallestIndex(List<Integer> list) {
         int low = 0;
         int high = list.size() - 1;
@@ -20,4 +24,37 @@ public class CircularSortedArraySolution {
 
         return low;
     }
+
+    // custom solution to variant 1
+    public static int searchGreatest(List<Integer> list) {
+        int low = 0;
+        int high = list.size() - 1;
+        int mid;
+        int midValue;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            midValue = list.get(mid);
+
+            if (midValue > list.get(low)) {
+                low = mid;
+                continue;
+            }
+
+            if (midValue < list.get(low)) {
+                high = mid - 1;
+                continue;
+            }
+
+            if (midValue < list.get(high)) {
+                low = mid + 1;
+                continue;
+            }
+
+            return mid;
+        }
+
+        return -1;
+    }
+
 }
--- src/test/java/com/james/io/search/CircularSortedArraySolutionTest.java
+++ src/test/java/com/james/io/search/CircularSortedArraySolutionTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class CircularSortedArraySolutionTest {
@@ -12,5 +13,29 @@ public class CircularSortedArraySolutionTest {
     public void testGetSmallestIndex() {
         List<Integer> list = Arrays.asList(378, 478, 550, 631, 103, 203, 220, 234, 279, 368);
         Assert.assertEquals(4, CircularSortedArraySolution.getSmallestIndex(list));
+    }
+
+    @Test
+    public void testSearchGreatestInMiddle() {
+        List<Integer> list = Arrays.asList(20, 30, 60, 5, 3);
+        Assert.assertEquals(2, CircularSortedArraySolution.searchGreatest(list));
+    }
+
+    @Test
+    public void testSearchGreatestInRight() {
+        List<Integer> list = Arrays.asList(100, 200, 500, 1000);
+        Assert.assertEquals(3, CircularSortedArraySolution.searchGreatest(list));
+    }
+
+    @Test
+    public void testSearchGreatestInLeft() {
+        List<Integer> list = Arrays.asList(2000, 3000, 100, 60, 59, 53, 50, 40, 30, 20, 1);
+        Assert.assertEquals(1, CircularSortedArraySolution.searchGreatest(list));
+    }
+
+    @Test
+    public void testSearchGreatestSingleton() {
+        List<Integer> list = Collections.singletonList(10);
+        Assert.assertEquals(0, CircularSortedArraySolution.searchGreatest(list));
     }
 }
