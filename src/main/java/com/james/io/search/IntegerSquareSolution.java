a3f8cdbd33d62816f19920561d2965405d8d9865
unknown <jsunthon@gmail.com>
2019-05-04 09:26:00 -0700

    add log n solution to 12.5 epi

---
+++ src/main/java/com/james/io/search/IntegerSquareSolution.java
@@ -0,0 +1,37 @@
+package com.james.io.search;
+
+public class IntegerSquareSolution {
+
+    public static int searchIntegerSquareRoot(int k) {
+        int i = 0;
+
+        while (Math.pow((double) i, 2) < k) {
+            if (Math.pow((double) i + 1, 2) > k) {
+                break;
+            }
+            i++;
+        }
+
+        return i;
+    }
+
+    public static int searchIntegerSquareRootLogN(int k) {
+        long low = 0;
+        long high = k;
+        long mid;
+        long midValue;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            midValue = mid * mid;
+
+            if (midValue > k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return (int) low - 1;
+    }
+}
---
+++ src/test/java/com/james/io/search/IntegerSquareSolutionTest.java
@@ -0,0 +1,57 @@
+package com.james.io.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegerSquareSolutionTest {
+
+    @Test
+    public void testSearchZero() {
+        Assert.assertEquals(0, IntegerSquareSolution.searchIntegerSquareRoot(0));
+    }
+
+    @Test
+    public void testSearchOne() {
+        Assert.assertEquals(1, IntegerSquareSolution.searchIntegerSquareRoot(1));
+    }
+
+    @Test
+    public void testSearchTwo() {
+        Assert.assertEquals(1, IntegerSquareSolution.searchIntegerSquareRoot(2));
+    }
+
+    @Test
+    public void testSearchThree() {
+        Assert.assertEquals(1, IntegerSquareSolution.searchIntegerSquareRoot(3));
+    }
+
+    @Test
+    public void testSearchSixteen() {
+        Assert.assertEquals(4, IntegerSquareSolution.searchIntegerSquareRoot(16));
+    }
+
+    @Test
+    public void testSearchZeroLogN() {
+        Assert.assertEquals(0, IntegerSquareSolution.searchIntegerSquareRootLogN(0));
+    }
+
+    @Test
+    public void testSearchOneLogN() {
+        Assert.assertEquals(1, IntegerSquareSolution.searchIntegerSquareRootLogN(1));
+    }
+
+    @Test
+    public void testSearchTwoLogN() {
+        Assert.assertEquals(1, IntegerSquareSolution.searchIntegerSquareRootLogN(2));
+    }
+
+    @Test
+    public void testSearchThreeLogN() {
+        Assert.assertEquals(1, IntegerSquareSolution.searchIntegerSquareRootLogN(3));
+    }
+
+    @Test
+    public void testSearchSixteenLogN() {
+        Assert.assertEquals(4, IntegerSquareSolution.searchIntegerSquareRootLogN(16));
+    }
+}
