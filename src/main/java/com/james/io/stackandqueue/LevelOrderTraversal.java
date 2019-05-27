1b57049e29cf6e38aec73cb4d2438614d05c4f49
--global <--global>
2019-05-26 20:13:03 -0700

    epi 9.7 solution

---
+++ src/main/java/com/james/io/stackandqueue/LevelOrderTraversal.java
@@ -0,0 +1,49 @@
+package com.james.io.stackandqueue;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * EPI 9.7 Problem
+ */
+public class LevelOrderTraversal {
+
+    static class BinaryTreeNode {
+        int val;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+
+        BinaryTreeNode(int val, BinaryTreeNode left, BinaryTreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<List<Integer>> traverse(BinaryTreeNode root) {
+        final List<List<Integer>> res = new ArrayList<>();
+        final Deque<BinaryTreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        int size = queue.size();
+        BinaryTreeNode curr;
+        List<Integer> subList;
+        while (!queue.isEmpty()) {
+            subList = new ArrayList<>();
+            while (size-- > 0) {
+                curr = queue.removeFirst();
+                subList.add(curr.val);
+                if (curr.left != null) {
+                    queue.addLast(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.addLast(curr.right);
+                }
+            }
+            res.add(subList);
+            size = queue.size();
+        }
+        return res;
+    }
+}
