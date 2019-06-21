5b311dd54f9c00f3d03dc67175de8207f485d397
--global <--global>
2019-06-21 08:31:53 -0700

    11.5 of EPI

---
+++ src/test/java/com/james/io/heap/RunningMedianTest.java
@@ -0,0 +1,26 @@
+package com.james.io.heap;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class RunningMedianTest {
+
+    @Test
+    public void genMedians() {
+        List<Integer> list = Arrays.asList(1, 0, 3, 5, 2, 0, 1);
+        List<Double> actualMedians = RunningMedian.genMedians(list.iterator());
+        List<Double> expectedMedians = Arrays.asList(1.0, 0.5, 1.0, 2.0, 2.0, 1.5, 1.0);
+        assertEquals(expectedMedians, actualMedians);
+    }
+
+    @Test
+    public void getMediansOneNum() {
+        assertEquals(Collections.singletonList(1.0),
+                RunningMedian.genMedians(Collections.singletonList(1).iterator()));
+    }
+}
