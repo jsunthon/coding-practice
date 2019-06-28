989e876bab78a0c507509464b60b895614a3787b
--global <--global>
2019-06-28 08:01:38 -0700

    10.2 of EPI

---
+++ src/main/java/com/james/io/binarytree/SymmetricTree.java
@@ -0,0 +1,46 @@
+package com.james.io.binarytree;
+
+/**
+ * 10.2 of EPI
+ */
+public class SymmetricTree {
+
+    static class BinaryTreeNode {
+        int value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+
+        BinaryTreeNode(int value) {
+            this(value, null, null);
+        }
+
+        BinaryTreeNode(int value, BinaryTreeNode left, BinaryTreeNode right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isSymmetric(BinaryTreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private static boolean isSymmetric(BinaryTreeNode left,
+                                       BinaryTreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left != null && right != null) {
+            return left.value == right.value
+                    && isSymmetric(left.left, right.right)
+                    && isSymmetric(left.right, right.left);
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/james/io/binarytree/SymmetricTreeTest.java
@@ -0,0 +1,94 @@
+package com.james.io.binarytree;
+
+import com.james.io.binarytree.SymmetricTree.BinaryTreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SymmetricTreeTest {
+
+    @Test
+    public void isSymmetricEmpty() {
+        assertTrue(SymmetricTree.isSymmetric(null));
+    }
+
+    @Test
+    public void isSymmetricRootOnly() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        assertTrue(SymmetricTree.isSymmetric(root));
+    }
+
+    @Test
+    public void isSymmetricLeftSkew() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        root.left = new BinaryTreeNode(300);
+
+        assertFalse(SymmetricTree.isSymmetric(root));
+    }
+
+    @Test
+    public void isSymmetricRightSkew() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        root.right = new BinaryTreeNode(300);
+
+        assertFalse(SymmetricTree.isSymmetric(root));
+    }
+
+    @Test
+    public void isSymmetricStructural() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        root.left = new BinaryTreeNode(299);
+        root.right = new BinaryTreeNode(300);
+
+        assertFalse(SymmetricTree.isSymmetric(root));
+    }
+
+    @Test
+    public void isSymmetric() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        root.left = new BinaryTreeNode(299);
+        root.right = new BinaryTreeNode(299);
+
+        assertTrue(SymmetricTree.isSymmetric(root));
+    }
+
+    @Test
+    public void isSymmetricLowerLevelsNot() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        BinaryTreeNode leftOfLeft = new BinaryTreeNode(1);
+        BinaryTreeNode rightOfLeft = new BinaryTreeNode(2);
+
+        root.left = new BinaryTreeNode(299, leftOfLeft, rightOfLeft);
+
+        BinaryTreeNode leftOfRight = new BinaryTreeNode(1);
+        BinaryTreeNode rightOfRight = new BinaryTreeNode(1);
+
+        root.right = new BinaryTreeNode(299, leftOfRight, rightOfRight);
+
+        assertFalse(SymmetricTree.isSymmetric(root));
+    }
+
+    @Test
+    public void isSymmetricLowerLevels() {
+        BinaryTreeNode root = new BinaryTreeNode(314);
+
+        BinaryTreeNode leftOfLeft = new BinaryTreeNode(3);
+        BinaryTreeNode rightOfLeft = new BinaryTreeNode(2);
+
+        root.left = new BinaryTreeNode(299, leftOfLeft, rightOfLeft);
+
+        BinaryTreeNode leftOfRight = new BinaryTreeNode(2);
+        BinaryTreeNode rightOfRight = new BinaryTreeNode(3);
+
+        root.right = new BinaryTreeNode(299, leftOfRight, rightOfRight);
+
+        assertTrue(SymmetricTree.isSymmetric(root));
+    }
+}
