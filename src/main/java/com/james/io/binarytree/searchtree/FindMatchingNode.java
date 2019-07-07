336ffa8a542d2ca8956871f7001cef39f8d6cc4c
--global <--global>
2019-07-07 14:39:55 -0700

    10.2 of EPI

---
+++ src/main/java/com/james/io/binarytree/searchtree/FindMatchingNode.java
@@ -0,0 +1,26 @@
+package com.james.io.binarytree.searchtree;
+
+import com.james.io.binarytree.model.BinaryTreeNode;
+
+/**
+ * Variant of 10.2 in EPI
+ *
+ * Time: O(h), h = height of the tree
+ * O(1) space
+ */
+public class FindMatchingNode {
+
+    public static BinaryTreeNode findFirstMatching(BinaryTreeNode root, int k) {
+        while (root != null) {
+            if (root.value == k) {
+                return root;
+            } else if (root.value < k) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+
+        return null;
+    }
+}
